4f60b2f0d7e410f06f46c0e4ba53df2e6351f9f4
Bruno Fondevila Rodriguez <BrunoFondevila@twinsoft.es>
2022-02-23 01:08:41 +0100

    Improved scene with island

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,22 +1,28 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xa0a0a0)
 // scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight()
-light.position.set(0.8, 1.4, 1.0)
-scene.add(light)
+// const light = new THREE.PointLight()
+// light.position.set(0.8, 1.4, 1.0)
+// scene.add(light)
 
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight)
+// const ambientLight = new THREE.AmbientLight(0xf5f5f3, 0.4)
+// scene.add(ambientLight)
 
-const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 1, 2000)
-camera.position.set(144, 133, 249)
+const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 1, 30000)
+camera.position.set(-770, 276, 793)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -25,70 +31,115 @@ controls.target.set(0, 1, 0)
 
 const clock = new THREE.Clock()
 
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.3)
+hemiLight.position.set(0, 200, 0)
+scene.add(hemiLight)
+
 const dirLight = new THREE.DirectionalLight(0xffffff)
-dirLight.position.set(0, 200, 100)
-dirLight.castShadow = true
-dirLight.shadow.camera.top = 180
-dirLight.shadow.camera.bottom = -100
-dirLight.shadow.camera.left = -120
-dirLight.shadow.camera.right = 120
+dirLight.position.set(500, 4000, 1000)
+// dirLight.castShadow = true
+dirLight.shadow.camera = new THREE.OrthographicCamera(-120, 120, 180, -100, 50, 1000)
 scene.add(dirLight)
 
-// ground
-const mesh = new THREE.Mesh(
-    new THREE.PlaneGeometry(2000, 2000),
-    new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
-)
-mesh.rotation.x = -Math.PI / 2
-mesh.receiveShadow = true
-scene.add(mesh)
+function createPathStrings(filename: string) {
+    const basePath = 'models/skybox/'
+    const baseFilename = basePath + filename
+    const fileType = '.bmp'
+    const sides = ['ft', 'bk', 'up', 'dn', 'rt', 'lf']
+    const pathStrings = sides.map((side) => {
+        return baseFilename + '_' + side + fileType
+    })
+    return pathStrings
+}
 
-const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000)
-const material = grid.material as THREE.Material
-material.opacity = 0.2
-material.transparent = true
-scene.add(grid)
+function createMaterialArray(filename: string) {
+    const skyboxImagepaths = createPathStrings(filename)
+    const materialArray = skyboxImagepaths.map((image) => {
+        let texture = new THREE.TextureLoader().load(image)
+        return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide })
+    })
+    return materialArray
+}
+
+function loadSkybox() {
+    const materialArray = createMaterialArray('Daylight')
+    const skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000)
+    const skybox = new THREE.Mesh(skyboxGeo, materialArray)
+    scene.add(skybox)
+}
 
 let mixer: THREE.AnimationMixer
 
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderConfig({ type: 'js' })
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+gltfLoader.setDRACOLoader(dracoLoader)
+
 const fbxLoader = new FBXLoader()
 
-fbxLoader.load(
-    'model/avatar.fbx',
-    (object) => {
-        object.castShadow = true
-        object.receiveShadow = true
-        object.scale.set(2, 2, 2)
-        object.position.x = -12
-
-        mixer = new THREE.AnimationMixer(object)
-        const clip = THREE.AnimationClip.findByName(object.animations, 'mixamo.com')
-        const action = mixer.clipAction(clip)
-        action.play()
-
-        object.traverse(function (child) {
-            if ((<THREE.Mesh>child).isMesh) {
-                child.castShadow = true
-                child.receiveShadow = true
-            }
+gltfLoader.load(
+    'models/island.gltf',
+    function (gltf) {
+        gltf.scenes.forEach((object) => {
+            object.castShadow = true
+            object.receiveShadow = true
+            object.scale.set(5, 5, 5)
+            object.traverse((child) => {
+                if ((<THREE.Mesh>child).isMesh) {
+                    child.castShadow = true
+                    child.receiveShadow = true
+                }
+            })
+            scene.add(object)
         })
-
-        scene.add(object)
+        loadSkybox()
+        loadAvatar()
     },
-    (xhr) => {
+    function (xhr) {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
     },
-    (error) => {
-        alert(error)
+    function (error) {
+        console.log('An error happened', error)
     }
 )
+
+function loadAvatar() {
+    fbxLoader.load(
+        'models/avatar.fbx',
+        (object) => {
+            object.castShadow = true
+            object.scale.set(2, 2, 2)
+            object.position.set(-500, 140, 500)
+
+            mixer = new THREE.AnimationMixer(object)
+            const clip = THREE.AnimationClip.findByName(object.animations, 'mixamo.com')
+            const action = mixer.clipAction(clip)
+            action.play()
+
+            object.traverse((child) => {
+                if ((<THREE.Mesh>child).isMesh) {
+                    child.castShadow = true
+                    child.receiveShadow = true
+                }
+            })
+
+            scene.add(object)
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            alert(error)
+        }
+    )
+}
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-
     render()
 }
 
